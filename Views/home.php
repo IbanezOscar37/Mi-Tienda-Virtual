4a84785b8a162f460b22a3ee7c9b92909eb5752d
Oscar Romero <fabian1037romero@gmail.com>
2024-02-02 20:16:12 -0500

    Mi proyecto ADSO en github

---
+++ Views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="ruta/a/tu/archivo.css">
+  <title>Mini Framework - OscarRomero</title>
+</head>
+<body>
+    <p>Mini Framework V1.0 - <a href="https://OscarRomero.com">www.OscarRomero.com</a></p>
+</body>
+</html> 
